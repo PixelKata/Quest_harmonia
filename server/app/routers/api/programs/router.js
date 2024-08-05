0f6d4094533096b0e5b953161e6733534aef29a6
PixelKata <guillaumebasilaire@gmail.com>
2024-08-05 16:26:45 +0200

    add path programs

---
+++ server/app/routers/api/programs/router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { browse } = require("../../../controllers/programActions");
+
+const router = express.Router();
+
+router.get("/", browse);
+
+module.exports = router;
